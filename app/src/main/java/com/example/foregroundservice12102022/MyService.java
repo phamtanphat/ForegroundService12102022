1631c1658b9d0331944e2764f81539363ad86fff
Phat <phatdroid94@gmail.com>
2022-12-14 18:28:35 +0700

    example play service

--- app/src/main/java/com/example/foregroundservice12102022/MyService.java
+++ app/src/main/java/com/example/foregroundservice12102022/MyService.java
@@ -10,11 +10,18 @@ import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.IBinder;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created by pphat on 12/12/2022.
@@ -23,6 +30,8 @@ public class MyService extends Service {
 
     final String CHANNEL_ID = "My channel";
     Notification notification;
+    List<Integer> listNumbers;
+    boolean isSort = false;
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -33,13 +42,48 @@ public class MyService extends Service {
     public void onCreate() {
         super.onCreate();
         notification = createNotification(this);
+        listNumbers = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            listNumbers.add(new Random().nextInt(100) + 1);
+        }
         startForeground(1, notification);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Toast.makeText(this, "onStartCommand", Toast.LENGTH_SHORT).show();
+        if (!isSort) {
+            isSort = true;
+            sortNumber(listNumbers, new OnSortListener() {
+                @Override
+                public void onFinish(List<Integer> newListNumber) {
+                    String textValue = TextUtils.join(", ", newListNumber);
+                    Log.d("pphat", textValue);
+                    isSort = false;
+                }
+            });
+        }
         return START_NOT_STICKY;
+    }
+
+    private void sortNumber(List<Integer> listNumbers, OnSortListener onSortListener) {
+        boolean isCorrect = true;
+        int indexPlus, tmpValue;
+        for (int i = 0; i < listNumbers.size(); i++) {
+            indexPlus = i + 1;
+            if (indexPlus >= listNumbers.size()) break;
+            if (listNumbers.get(i) > listNumbers.get(indexPlus)) {
+                tmpValue = listNumbers.get(indexPlus);
+                listNumbers.set(indexPlus, listNumbers.get(i));
+                listNumbers.set(i, tmpValue);
+                isCorrect = false;
+            }
+        }
+
+        if (!isCorrect) {
+            sortNumber(listNumbers, onSortListener);
+        } else {
+            onSortListener.onFinish(listNumbers);
+        }
     }
 
     @Override
@@ -54,7 +98,7 @@ public class MyService extends Service {
                 .setSmallIcon(R.drawable.ic_launcher_background)
                 .setContentTitle("Thông báo")
                 .setContentText("Service đang chạy")
-                .setSound(sound)
+//                .setSound(sound)
                 .setPriority(NotificationCompat.PRIORITY_HIGH);
 
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
@@ -64,5 +108,9 @@ public class MyService extends Service {
             notificationManager.createNotificationChannel(notificationChannel);
         }
         return builder.build();
+    }
+
+    interface OnSortListener {
+        void onFinish(List<Integer> newList);
     }
 }
